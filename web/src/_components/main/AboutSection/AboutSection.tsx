e555c8f746e04cab869a76817cfe52667af3ea49
luka.gorgadze <luka.g.gorgadze@gmail.com>
2025-08-22 12:08:49 +0400

    Fixed Social links and about section

--- web/src/_components/main/AboutSection/AboutSection.tsx
+++ web/src/_components/main/AboutSection/AboutSection.tsx
@@ -2,12 +2,12 @@ import styles from "./AboutSection.module.css";
 
 const AboutSection = () => {
     return (
-        < section className={styles.aboutSection} id="about" >
+        <section className={styles.aboutSection} id="about">
             <div className={styles.aboutContent}>
                 <h2 className={styles.aboutHeadline}>About Branvia</h2>
                 <div>
                     <p className={styles.aboutText}>
-                        Branvia is dedicated to empowering small businesses with high-quality, AI-generated image prompts tailored for premium marketing. Our mission is to make luxury-level visual content accessible, creative, and effective for small businesses.
+                        Branvia is dedicated to empowering small businesses and individuals with high-quality, AI-generated marketing visuals and prompts tailored for premium branding. Our mission is to make luxury-level visual content accessible, creative, and effective for everyone.
                     </p>
                     <p className={styles.contactText}>
                         For any questions or help, contact us at: <a href="mailto:info@branvia.art" className={styles.contactEmail}>info@branvia.art</a>
@@ -15,7 +15,7 @@ const AboutSection = () => {
                 </div>
 
             </div>
-        </section >
+        </section>
     )
 }
 
